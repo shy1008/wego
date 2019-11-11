a6ff5e1fb4caaee9fbffff81d14bd9baea1d5e2e
shy1008 <46985746+shy1008@users.noreply.github.com>
2019-11-11 12:37:10 +0900

    mobile메인완료
    11/11모바일메인레이아웃완료

--- index.php
+++ index.php
@@ -141,9 +141,9 @@
         <footer>
           <span style="padding:30px 0px; line-height: 80px; font-size:24px;">STAY UP TO DATE ON GODS UNCHAINED<span><br/>
           <ul class="footer_icon">
-            <li><img src="./img/item/icon1.png" alt="twitter" style="width:50px;"></li>
-            <li><img src="./img/item/icon2_1.png" alt="discord" style="width:50px;"></li>
-            <li><img src="./img/item/icon3_1.png" alt="reddit" style="width:50px;"></li>
+            <li><img src="./img/icon/icon1.png" alt="twitter" style="width:50px;"></li>
+            <li><img src="./img/icon/icon2_1.png" alt="discord" style="width:50px;"></li>
+            <li><img src="./img/icon/icon3_1.png" alt="reddit" style="width:50px;"></li>
           </ul>
           <ul class="footer_menu">
             <li>Blog</li>
--- m/index.php
+++ m/index.php
@@ -62,13 +62,72 @@
         <div class="section_text" data-aos="fade-up">
           <span>TOURNAMENT PRIZEPOOL</span><br/>10% of pack sales go directly to the first Gods Unchained World Championship, with a $1.6M goal.
         </div>
-        <div class="left_box" data-aos="fade-up" duration: 3000>
-            <picture>
-              <source srcset="../img/card.webp" type="image/webp" /> 
-            </picture>
-          </div>
+        <div class="left_box" data-aos="fade-up" >
+           <img src="../img/card.png" alt="">
+        </div>
+        <div class="section_text2" data-aos="fade-up">
+          <span>Exclusive Presale Packs</span><br/>A completely limited edition Genesis Season of 380 unique cards. Only available before game launch, and hard-capped at $15M. Don’t miss out.
+        </div>
+
+        <div class="videowrapper">
+            <iframe  src="https://www.youtube.com/embed/H4CuvWJd8kE?"frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </div>
     </section>
+<!-- 단락 -->
+    <section class="section4"> 
+      <div class="right_text_content" data-aos="fade-up">
+        <span style="font-size:35px; font-weight:bold; margin-bottom:50px;">SEAMLESS GAMING.</span>
+          <p><br/>Magic: The Gathering created a genre. <br/>
+          Hearthstone redefined it in a digital format.<br/>
+          We’re taking the best elements from each game and expanding upon <br/>
+          them in new ways to create a gameplay experience <br/>
+          which feels strangely classic yet refreshing.</p>
+      </div>
+    </section>
+<!-- 단락 -->
+    <section class="section5"> 
+      <div class="right_text_content" data-aos="fade-up">
+        <span style="font-size:35px; font-weight:bold; margin-bottom:50px;">SEAMLESS GAMING.</span>
+          <p><br/>Magic: The Gathering created a genre. <br/>
+          Hearthstone redefined it in a digital format.<br/>
+          We’re taking the best elements from each game and expanding upon <br/>
+          them in new ways to create a gameplay experience <br/>
+          which feels strangely classic yet refreshing.</p>
+      </div>
+    </section>
+<!-- 단락 -->
+    <section class="section6"> 
+      <div class="right_text_content" data-aos="fade-up">
+        <span style="font-size:35px; font-weight:bold; margin-bottom:50px;">SEAMLESS GAMING.</span>
+          <p><br/>Magic: The Gathering created a genre. <br/>
+          Hearthstone redefined it in a digital format.<br/>
+          We’re taking the best elements from each game and expanding upon <br/>
+          them in new ways to create a gameplay experience <br/>
+          which feels strangely classic yet refreshing.</p>
+      </div>
+    </section>
+<!-- 단락 -->
+
+    <!-- 푸터시작 -->
+    <footer>
+      <span style="padding:40px 0px; line-height: 40px; font-size:16px;">STAY UP TO DATE ON GODS UNCHAINED<span><br/>
+          <ul class="footer_icon">
+            <li><img src="..//img/icon/icon1.png" alt="twitter" style="width:50px;"></li>
+            <li><img src="../img/icon/icon2_1.png" alt="discord" style="width:50px;"></li>
+            <li><img src="../img/icon/icon3_1.png" alt="reddit" style="width:50px;"></li>
+          </ul>
+          <ul class="footer_menu">
+            <li>Blog</li>
+            <li>Referrals</li>
+            <li>FAQ</li>
+            <li>Terms</li>
+            <li>Activate</li>
+            <li>Jobs</li>
+          </ul>
+          <p style="font-size:12px;">Copyright © 2019 Immutable. All rights reserved.</p>
+    <!-- 푸터끝 -->
+    </footer>
   
 
 <!-- wrap끝 -->
