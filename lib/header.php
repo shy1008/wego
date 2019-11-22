58c61062743ac045ef41fde844df77f02fa0b920
shy1008 <46985746+shy1008@users.noreply.github.com>
2019-11-22 17:44:05 +0900

    수정 마무리 추가
    11/22 게임파일,로고이미지 수정

--- lib/header.php
+++ lib/header.php
@@ -1,6 +1,6 @@
 <nav>
     <ul class="menu_left">
-        <li><a href="./index.php"><img src="./img/logo.jpg" alt="logo" style="width:32px; opacity: 1;"></a></li>
+        <li><a href="./index.php"><img src="./img/Logo.png" alt="logo" style="width:32px; opacity: 1;"></a></li>
         <li><a href="./index.php"><img src="./img/icon/32/home.png" alt="sub_menu1"></a></li>
         <li><a href="./gallery.php"><img src="./img/icon/32/compass.png" alt="sub_menu2"></a></li>
         <li class="open"><a href="./login.php"><img src="./img/icon/32/bag.png" alt="sub_menu3"></a></li>
@@ -11,3 +11,4 @@
         <li><a href="./login.php"><img src="./img/down4.png" alt="download" style="height:35px; cursor:pointer; opacity:.8;"></a> </li>
     </ul>
 </nav>
+<!-- https://codepen.io/elenanicole53/pen/rsiDj -->
